b136c374705261d341a8d96184d02e6e52a58e5a
Matthew Oshimo <moshimo90696@gmail.com>
2024-06-11 21:34:11 -0700

    now has the color style of a given page change to red when you go to the page.

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ async function getWebsite() {
   const prompt = require('prompt-sync')({'fake_val':' '});
   
   let site = prompt('Enter a website (full url): ');
+  let highlight = prompt('Do you want to hightlight links? Yes or No: ');
+  highlight.toLowerCase();
+  while(highlight != 'yes' && highlight != 'no'){
+    console.log(highlight);
+    console.log('Please answer with yes or no.');
+    highlight = prompt('Do you want to highlight links? Yes or No: ');
+  }
   // launch browser
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
@@ -12,9 +19,15 @@ async function getWebsite() {
 
   // go to Hacker News
   await page.goto(`${site}`);
-  await page.evaluate(([]) => {
-
-}, []);
+  await page.evaluate(([data]) => {
+    if(data == 'yes'){
+      const lister = Array.from(document.getElementsByTagName('body'));
+      lister.map((unit) => {
+        console.log("Changer!");
+        unit.style.color = 'red';}
+      );
+    }
+}, [highlight]);
 
 }
 
